23df40c643ecf72ca185f1d2d4f2f65ae5b06295
farukayaz <frkyz07.13@gmail.com>
2022-10-02 00:35:38 +0300

    - tax should be fixed

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -17,78 +17,71 @@ public class Employee {
 
     public double tax(Double salary){
 
-        this.salary = salary;
         double tax = 0;
         if(salary>1000){
             tax = (salary*3)/100;
-        }
-        return tax;
+            return tax;
+        }else
+            return 0;
     }
-    public double bonus(double workHours,double salary){
-        this.salary = salary;
-        this.workHours = workHours;
+    public double bonus(double workHours,double salary) {
+
         double hourlyPay = 30;
         double extraHours = 0;
-        double salaryWithBonus = 0;
+        double bonus = 0;
 
-        if(workHours>40){
+        if (workHours > 40) {
             extraHours = workHours - 40;
-            salaryWithBonus = (extraHours*hourlyPay)+salary;
-            return salaryWithBonus;
-        }else{
-            return salary;
+            bonus = (extraHours * hourlyPay);
         }
-
+        return bonus;
     }
 
     public double raiseSalary(int year,double salary,double workHours){
-        this.workHours = workHours;
-        this.year = year;
-        this.salary = salary;
 
         int currentDate = 2021;
-        double newSalary = 0;
-        double raisedSalary =0;
-        double bonusAmounth =0;
+        double raisedSalaryAmounth =0;
+        double raisedSalary=0;
+
         int workYear = currentDate - year;
 
-
         if (workYear<10){
-            raisedSalary = salary*1.05;
+            raisedSalaryAmounth = salary*0.05;
         }
         else if (workYear>9 && workYear<20 ) {
-            raisedSalary = salary*1.10;
+            raisedSalaryAmounth = salary*0.10;
         }
         else if (workYear>19){
-            raisedSalary = salary*1.15;
+            raisedSalaryAmounth = salary*0.15;
         }
+        raisedSalary = (raisedSalaryAmounth+bonus(workHours,salary)+salary);
 
-        bonusAmounth = bonus(workHours,salary)- salary;
-        newSalary = bonus(workHours,raisedSalary); // salary'e bonus eklendi
-        newSalary = newSalary-tax(newSalary); // tax düşüldü
-
-        System.out.println("Bonus amounth "+bonusAmounth);
-        System.out.println("Salary after bonus and raise "+bonus(workHours,raisedSalary));
-        System.out.println("Salary tax "+ tax(newSalary));
-        System.out.println("Total Amount of raise "+(newSalary-salary));
-        System.out.println("New salary "+newSalary);
-
-        return newSalary;
-
+        return raisedSalary-tax(raisedSalary);
     }
 
-    public void information(){
+    public String toString(){
+
+        double totalRaise = (bonus(workHours,salary)+(raiseSalary(year,salary,workHours)-salary));
+        double raisedSalary = raiseSalary(year,salary,workHours);
+        double tax = tax(raisedSalary);
+        double raisedAmounth = raiseSalary(year,salary,workHours)-salary;
+        double newSalary = raisedSalary;
+
         System.out.println("Employee name: "+name);
         System.out.println("Employee salary: "+salary);
         System.out.println("Employee workHours: "+workHours);
         System.out.println("Employee start year: "+year);
-        System.out.println("Employee new salary:"+raiseSalary(year,salary,workHours));
+        System.out.println("Salary tax: "+ tax);
+        System.out.println("Bonus amounth: "+bonus(workHours,salary));
+        System.out.println("Raised amounth: "+raisedAmounth);
+        System.out.println("Salary after bonus and raise: "+raisedSalary);
+        System.out.println("New salary: "+newSalary);
+
+        return null;
     }
 
-
     public static void main(String[] args) {
-        Employee ee = new Employee("ahmet", 3000.0,50.0,2005);
-        ee.information();
-
+        Employee ee = new Employee("ahmet", 1000.0,40.0,2010);
+        ee.toString();
     }
 }
